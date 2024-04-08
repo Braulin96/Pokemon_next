aeb656471a5e39c7ee35bee9015dbc16b7f0ca36
Braulin Pires <braulinpires@hotmail.com>
2024-04-08 20:42:24 +0100

    added others file routes

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <p className="text-red-500">here goes all our API to get the pokemon</p>
+        
       </div>   
     </main>
   );
